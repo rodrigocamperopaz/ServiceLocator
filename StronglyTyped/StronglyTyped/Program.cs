6de87e5e961524a106bda6280b5bd5eaa1590c01
rodrigocamperopaz <rodrigo.campero@trueforce.com>
2022-01-23 06:10:03 -0400

    Adding service locator types

---
+++ StronglyTyped/StronglyTyped/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Client
+{
+    public interface IService
+    {
+        void ExecuteService();
+    }
+    public class LoggingService : IService
+    {
+        public void ExecuteService()
+        {
+            Console.WriteLine("Executing Log Service");
+        }
+    }
+    public interface IServiceLocator
+    {
+        IService LoggingService { get; set; }
+    }
+
+    public class ServiceLocator : IServiceLocator
+    {
+        private IService? ObjService;
+
+        //Service locator function returning strong type   
+        public IService LoggingService
+        {
+            get
+            {
+                if (ObjService == null) return new LoggingService();
+                return ObjService;
+            }
+            set { ObjService = value; }
+        }
+    }
+
+    public class ClassA
+    {
+        private IService? ObjService;
+
+        public ClassA(IServiceLocator serviceLocator)
+        {
+            ObjService = serviceLocator.LoggingService;
+        }
+
+        //Execute service  
+        public void ExecuteService()
+        {
+            ObjService?.ExecuteService();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceLocator serviceLocator = new ServiceLocator();
+            serviceLocator.LoggingService = new LoggingService();
+
+            ClassA classA = new ClassA(serviceLocator);
+
+            classA.ExecuteService();
+            Console.ReadKey();
+        }
+    }
+}
